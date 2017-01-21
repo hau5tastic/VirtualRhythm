f64b99b48f891f8a0df2cb30985f4ba3da1e0419
BountyXSnipe <kevinmaspogi@hotmail.com>
2017-01-21 04:25:28 -0500

    A lot of stuff from stream
    Includes:
    -Different note colors
    -Fully functioning 8 dual directional input
    -Able to destroy notes with proper timing
    -Score grading based on how close the notes were to the panels

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,43 +2,129 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
 
     //These variables retrieve the input value from 0 to 1
-    public float horizontalInput;
-    public float verticalInput;
-    public float horizontalInput2;
-    public float verticalInput2;
-
     public Vector2 stickInput;
     public Vector2 stickInput2;
 
+    public TriggerBoxes[] triggerBoxes;
+
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+        //StartCoroutine("HighlightTriggerPanels2");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        GetJoyInputs();
+        HighlightTriggerPanels();
 
-        horizontalInput2 = Input.GetAxis("Horizontal 2");
-        verticalInput2 = Input.GetAxis("Vertical 2");
+        for (int i = 0; i < triggerBoxes.Length; i++) //Use a script so we don not have to use GetComponent every frame
+        {
+            if (stickInput.x == (triggerBoxes[i].highlightReq.x) && stickInput.y == (triggerBoxes[i].highlightReq.y))
+            {
+                triggerBoxes[i].highlighted = true;
+            }
+            else
+            {
+                triggerBoxes[i].highlighted = false;
+            }
 
-
-        float deadzone = 0.25f;
-        stickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (stickInput.magnitude < deadzone)
-            stickInput = Vector2.zero;
-        else
-            stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
-
-        stickInput2 = new Vector2(Input.GetAxis("Horizontal 2"), Input.GetAxis("Vertical 2"));
-        if (stickInput.magnitude < deadzone)
-            stickInput = Vector2.zero;
-        else
-            stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
+            if (stickInput2.x == (triggerBoxes[i].highlightReq.x) && stickInput2.y == (triggerBoxes[i].highlightReq.y))
+            {
+                triggerBoxes[i].highlighted2 = true;
+            }
+            else
+            {
+                triggerBoxes[i].highlighted2 = false;
+            }
+        }
     }
+
+    void GetJoyInputs()
+    {
+        float deadzone = 0.25f;
+        stickInput = new Vector2(Mathf.Round(Input.GetAxis("Horizontal")), Mathf.Round(Input.GetAxis("Vertical")));
+        /*
+        if (stickInput.magnitude < deadzone)
+            stickInput = Vector2.zero;
+        else
+            stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
+            */
+        stickInput2 = new Vector2(Mathf.Round(Input.GetAxis("Horizontal 2")), Mathf.Round(Input.GetAxis("Vertical 2")));
+        /*
+        if (stickInput.magnitude < deadzone)
+            stickInput = Vector2.zero;
+        else
+            stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
+            */
+
+    }
+
+    void HighlightTriggerPanels()
+    {
+        /*
+        //Top panel
+        if (stickInput.y > 0.5f && stickInput2.y > 0.5f)
+        {
+            triggerBoxes[0].highlighted = true;
+            triggerBoxes[0].highlighted2 = true;
+        }
+        else if (stickInput.y > 0.5f)
+            triggerBoxes[0].highlighted = true;
+        else if (stickInput2.y > 0.5f)
+            triggerBoxes[0].highlighted2 = true;
+
+        //Top Right panel
+        
+        if (stickInput.y > 0.5f && stickInput.x > 0.5f
+            && stickInput2.y > 0.5f && stickInput2.x > 0.5f)
+        {
+            triggerBoxes[1].highlighted = true;
+            triggerBoxes[1].highlighted2 = true;
+        }
+        else if (stickInput.y > 0.5f)
+            triggerBoxes[1].highlighted = true;
+        else if (stickInput2.y > 0.5f)
+            triggerBoxes[1].highlighted2 = true;
+            
+        if (stickInput.x > 0.5f)
+        {
+            triggerBoxes[2].highlighted = true;
+        }
+        else
+        {
+            triggerBoxes[2].highlighted = false;
+        }
+        */
+    }
+
+    IEnumerator HighlightTriggerPanels2()
+    {
+        Debug.Log("Co started");
+        while (true)
+        {
+            Debug.Log("Co running");
+            for (int i = 0; i < triggerBoxes.Length; i++) //Use a script so we don not have to use GetComponent every frame
+            {
+                if (stickInput.x == triggerBoxes[i].highlightReq.x && stickInput.y == triggerBoxes[i].highlightReq.y)
+                {
+                    triggerBoxes[i].highlighted = true;
+                }
+                else
+                {
+                    triggerBoxes[i].highlighted = false;
+                }
+            }
+            yield return null;
+        }
+    }
+
 }
+
+
+
---
+++ Assets/Scripts/TriggerBoxes.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBoxes : MonoBehaviour {
+
+    public bool highlighted = false; //Becomes active when the joystick aims towards it
+    public bool highlighted2 = false; //Becomes active when the other joystick aims towards it
+
+    public PlayerScript pScript;
+
+    public Vector2 highlightReq; //This is the value which will check input in order to highlight the panel
+    public bool collidedWithNote = false;
+    private GameObject note;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HighlightState();
+
+        //Check if the collider is a note
+        if (note != null && collidedWithNote == true)
+        {
+            //If purple highlight
+            if (highlighted == true && highlighted2 == true)
+            {
+                //Due to the natural human inability to press both buttons at the exact same time, we need to have multiple
+                //conditional branches to allow one button to be pressed and then the other to register the hit
+                if (Input.GetButtonDown("Left Fire") && Input.GetButtonDown("Right Fire"))
+                {
+                    //Process destroying the note
+                    if (note.tag == "Note" || note.tag == "Purple Note")
+                        DestroyNote(note);
+                }
+                else if (Input.GetButton("Left Fire"))
+                {
+                    //Process destroying the note
+                    if (Input.GetButtonDown("Right Fire"))
+                        if (note.tag == "Note" || note.tag == "Purple Note")
+                            DestroyNote(note);
+                }
+                else if (Input.GetButton("Right Fire"))
+                {
+                    //Process destroying the note
+                    if (Input.GetButtonDown("Left Fire"))
+                        if (note.tag == "Note" || note.tag == "Purple Note")
+                            DestroyNote(note);
+                }
+            }
+            else if (highlighted == true) //blue highlight
+            {
+                if (Input.GetButtonDown("Left Fire"))
+                {
+                    //Process destroying the note
+                    if (note.tag == "Note" || note.tag == "Blue Note")
+                        DestroyNote(note);
+                }
+
+            }
+            else if (highlighted2 == true) //orange highlight
+            {
+                if (Input.GetButtonDown("Right Fire"))
+                {
+                    //Process destroying the note
+                    if (note.tag == "Note" || note.tag == "Orange Note")
+                        DestroyNote(note);
+                }
+            }
+        }
+    }
+
+    //This method will check joystick input to highlight the panels
+    void HighlightInput()
+    {
+        if (pScript.stickInput.x != 0 && pScript.stickInput.y != 0)
+        {
+            //If a right panel
+            if (highlightReq.x > 0)
+            {
+                //No vertical
+                if (highlightReq.y == 0)
+                {
+                    if (pScript.stickInput.x > 0.5f)
+                        highlighted = true;
+                }
+                else if (highlightReq.y > 0)
+                {
+                    if (pScript.stickInput.x > 0.5f && pScript.stickInput.y > 0.5f);
+                }
+            }
+        }
+    }
+
+    void HighlightState()
+    {
+        if (highlighted == true && highlighted2 == true)
+        {
+            //turn purple
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (highlighted == true)
+        {
+            //turn blue
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else if (highlighted2 == true)
+        {
+            //turn orange
+            GetComponent<Renderer>().material.color = new Color(1, 0.55f, 0, 1);
+        }
+        else if (highlighted == true && highlighted2 == true)
+        {
+            //turn purple
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else
+        {
+            //turn purple
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        collidedWithNote = true;
+        note = c.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+        collidedWithNote = false;
+        note = null;
+    }
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        /*
+        //Check if the collider is a note
+        if (c.tag == "Note")
+        {
+            //Debug.Log("Collided");
+            if (highlighted == true)
+            {
+                if (Input.GetButtonDown("Left Fire"))
+                {
+                    //Process destroying the note
+                    DestroyNote(c.gameObject);
+                }
+
+            }
+            else if (highlighted2 == true)
+            {
+                if (Input.GetButtonDown("Right Fire"))
+                {
+                    //Process destroying the note
+                    DestroyNote(c.gameObject);
+                }
+            }
+        }
+        */
+    }
+
+    void DestroyNote(GameObject note)
+    {
+        //Check the distance between the note and the panel
+        float distance = Vector2.Distance(transform.position, note.transform.position);
+        if (distance < 0.1f)
+        {
+            Debug.Log("Perfect!");
+        }
+        else if (distance < 0.25f)
+        {
+            Debug.Log("Great!");
+        }
+        else if (distance < 0.5f)
+        {
+            Debug.Log("Good!");
+        }
+        else if (distance < 1)
+        {
+            Debug.Log("Ok");
+        }
+        //Destroy the note
+        Destroy(note.gameObject);
+    }
+}
