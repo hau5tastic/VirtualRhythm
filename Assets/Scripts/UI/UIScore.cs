53c477aec7459e45412b6a46947d910228b97649
Nicco Jacinto <nix_jacinto25@hotmail.com>
2017-01-21 18:01:57 -0500

    Initial

---
+++ Assets/Scripts/UI/UIScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Texture))]
+public class UIScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
